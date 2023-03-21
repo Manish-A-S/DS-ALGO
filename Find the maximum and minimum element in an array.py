fdb314ef875455a2ea87e1fe513ab6255886177a
Manish-A-S <119799281+Manish-A-S@users.noreply.github.com>
2023-03-21 22:46:00 +0530

    Add files via upload

---
+++ Find the maximum and minimum element in an array.py
@@ -0,0 +1,12 @@
+nums=[1,7,2,9,8,4,6]
+min=nums[0]
+max=nums[0]
+for i in range(1,len(nums)):
+    if nums[i]>max:
+        max=nums[i]
+    if nums[i]<min:
+        min=nums[i]
+
+nums.remove(max)
+nums.remove(min)
+print(nums)
